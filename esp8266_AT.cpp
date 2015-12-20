e3bcc8d6d8b3f0944562855f42e993746b212486
Sergey Prilukin <sprilukin@gmail.com>
2015-12-20 11:18:12 +0200

    refactoring of the sample

--- esp8266_AT.cpp
+++ esp8266_AT.cpp
@@ -9,34 +9,7 @@
 #include <Esp8266_AT.h>
 #include <Stream.h>
 
-//wifi connection mode: AT+CWMODE=<mode>
-//mode: 1 — STA, 2 — AP, 3 — BOTH
-
-#define AT_WIFI_CONNECTION_MODE_STA "AT+CWMODE=1"
-#define AT_WIFI_CONNECTION_MODE_AP "AT+CWMODE=2"
-#define AT_WIFI_CONNECTION_MODE_BOTH "AT+CWMODE=3"
-
-//wifi connect to AP command: AT+CWJAP="<ap name>","<password>"
-#define AT_AP_CONNECT "AT+CWJAP="
-
-//multiple connections mode AT+CIPMUX=<mode>
-//mode: 0 - single connection, 1- multiple connection
-#define SINGLE_CONNECTION_MODE "AT+CIPMUX=0"
-#define MULTI_CONNECTION_MODE "AT+CIPMUX=1"
-
-//tcp connection start: AT+CIPSTART="TCP","8.8.8.8",80
-#define TCP_START_CONNECTION "AT+CIPSTART="
-//start sending message over tcp
-#define TCP_SEND_MESSAGE "AT+CIPSEND="
-//prompt message after which we can start sending our request
-#define TCP_SEND_START_PROMPT ">"
-//close tcp connection
-#define TCP_CLOSE_CONNECTION "AT+CIPCLOSE"
-
-//AT OK response
-#define AT_RESPONSE_OK "OK"
-
-Esp8266AT::Esp8266AT(Stream* stream, Stream* dbgStream) {
+Esp8266AT::Esp8266AT(Stream *stream, Stream *dbgStream) {
     this->_stream = stream;
     this->_dbgStream = dbgStream;
 }
@@ -53,26 +26,32 @@ boolean Esp8266AT::setup(String accessPoint, String password) {
 
 boolean Esp8266AT::executeCommandAndWaitForResult(String command, String expectedResult) {
     this->_stream->println(command);
-    this->_dbgStream->print(command);
+
+    if (this->_dbgStream) {
+        this->_dbgStream->print(command);
+    }
 
     char expectedResultCharArray[expectedResult.length()];
     expectedResult.toCharArray(expectedResultCharArray, expectedResult.length());
 
-    if (this->_stream->find(expectedResultCharArray)){
-        this->_dbgStream->println(": OK");
+    if (this->_stream->find(expectedResultCharArray)) {
+        if (this->_dbgStream) {
+            this->_dbgStream->println(": OK");
+        }
+        this->_finishLastCommand();
         return true;
-    }else{
-        this->_dbgStream->println(": Error");
+    } else {
+        if (this->_dbgStream) {
+            this->_dbgStream->println(": Error");
+        }
+        this->_finishLastCommand();
         return false;
     }
 }
 
 void Esp8266AT::executeCommandAndIgnoreResult(String command) {
     this->_stream->println(command);
-    this->_dbgStream->println(command);
-    while (this->_stream->available()) {
-        this->_dbgStream->write(this->_stream->read());
-    }
+    this->_finishLastCommand();
 }
 
 boolean Esp8266AT::get(String ip, int port, String request, String expectedResult) {
@@ -85,7 +64,9 @@ boolean Esp8266AT::get(String ip, int port, String request, String expectedResul
         return false;
     }
 
-    if (!this->executeCommandAndWaitForResult(String(TCP_SEND_MESSAGE) + String(request.length()), TCP_SEND_START_PROMPT)) {
+    if (!this->executeCommandAndWaitForResult(
+            String(TCP_SEND_MESSAGE) + String(request.length()),
+            TCP_SEND_START_PROMPT)) {
         this->executeCommandAndIgnoreResult(TCP_CLOSE_CONNECTION);
         return false;
     }
@@ -94,9 +75,20 @@ boolean Esp8266AT::get(String ip, int port, String request, String expectedResul
         this->executeCommandAndIgnoreResult(TCP_CLOSE_CONNECTION);
         return false;
     }
+}
 
+void Esp8266AT::_finishLastCommand() {
     //Just for debugging purposes to read whole response
-    while (this->_stream->available()) {
-        this->_dbgStream->write(this->_stream->read());
+    if (this->_dbgStream) {
+        while (this->_stream->available()) {
+            this->_dbgStream->write(this->_stream->read());
+        }
+    } else {
+        while (this->_stream->available()) {
+            this->_stream->read();
+        }
     }
+
+    //delay between commands to allow chip to finish last command
+    delay(DELAY_BETWEEN_COMMANDS);
 }
--- esp8266_AT.h
+++ esp8266_AT.h
@@ -8,6 +8,36 @@
 #ifndef Esp8266AT_h
 #define Esp8266AT_h
 
+//wifi connection mode: AT+CWMODE=<mode>
+//mode: 1 — STA, 2 — AP, 3 — BOTH
+#define AT_WIFI_CONNECTION_MODE_STA "AT+CWMODE=1"
+#define AT_WIFI_CONNECTION_MODE_AP "AT+CWMODE=2"
+#define AT_WIFI_CONNECTION_MODE_BOTH "AT+CWMODE=3"
+
+//wifi connect to AP command: AT+CWJAP="<ap name>","<password>"
+#define AT_AP_CONNECT "AT+CWJAP="
+
+//multiple connections mode AT+CIPMUX=<mode>
+//mode: 0 - single connection, 1- multiple connection
+#define SINGLE_CONNECTION_MODE "AT+CIPMUX=0"
+#define MULTI_CONNECTION_MODE "AT+CIPMUX=1"
+
+//tcp connection start: AT+CIPSTART="TCP","8.8.8.8",80
+#define TCP_START_CONNECTION "AT+CIPSTART="
+//start sending message over tcp
+#define TCP_SEND_MESSAGE "AT+CIPSEND="
+//prompt message after which we can start sending our request
+#define TCP_SEND_START_PROMPT ">"
+//close tcp connection
+#define TCP_CLOSE_CONNECTION "AT+CIPCLOSE"
+
+//AT OK response
+#define AT_RESPONSE_OK "OK"
+
+
+#define DELAY_BETWEEN_COMMANDS 1000 //1 second delay between commands
+
+
 #include "Arduino.h"
 #include <Stream.h>
 
@@ -22,7 +52,7 @@ public:
      * stream - serial port connected to esp8266 chip
      * dbgStream - serial port for debug purposes
      */
-    Esp8266AT(Stream *stream, Stream *dbgStream);
+    Esp8266AT(Stream *stream, Stream *dbgStream = NULL);
     boolean executeCommandAndWaitForResult(String command, String expectedResult);
     void executeCommandAndIgnoreResult(String command);
     boolean setup(String accessPoint, String password);
@@ -32,6 +62,7 @@ public:
 private:
     Stream *_stream;
     Stream *_dbgStream;
+    void _finishLastCommand();
 };
 
 #endif
